7e91ab2be752aabfafe60ddb87c9865b96280bb9
Akarnik0 <brestovacnikola5@gmail.com>
2025-05-18 14:23:14 +0200

    Add project files.

---
+++ BibliotekaNikola/Form2.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace BibliotekaNikola
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.bibliotekaDataSet = new BibliotekaNikola.BibliotekaDataSet();
+            this.bibliotekaDataSet1 = new BibliotekaNikola.BibliotekaDataSet();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.bibliotekaDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bibliotekaDataSet1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(124, 79);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(94, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(161, 273);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 38);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(124, 119);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(177, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // bibliotekaDataSet
+            // 
+            this.bibliotekaDataSet.DataSetName = "BibliotekaDataSet";
+            this.bibliotekaDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // bibliotekaDataSet1
+            // 
+            this.bibliotekaDataSet1.DataSetName = "BibliotekaDataSet";
+            this.bibliotekaDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(67, 181);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(67, 50);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(140, 181);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(67, 50);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(213, 181);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(67, 50);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(286, 181);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(67, 50);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(183, 353);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "label1";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.bibliotekaDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bibliotekaDataSet1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private BibliotekaDataSet bibliotekaDataSet;
+        private BibliotekaDataSet bibliotekaDataSet1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ BibliotekaNikola/Form2.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//NAPOMENA!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+//ovo dvoje je potrebno dodati u svaku formu
+//potrebni su za izvrsavanje funkcija i unos podataka u bazi
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace BibliotekaNikola
+{
+    public partial class Form2 : Form
+    {
+
+        //stvaranje veze sa bazom
+        SqlConnection veza = new SqlConnection(@"Data Source =.\sqlexpress; Initial Catalog = Biblioteka; Integrated Security = True; Encrypt = False");
+        //adapter,dataset,dataview,currencymanager i komanda za bazu
+        //pakistanac nije dobro objasnio currencymanager
+        SqlDataAdapter adapter;
+        DataSet pisac;
+        DataView pregled;
+        CurrencyManager trenutni;
+        string unospisaca = "select * from Pisac";
+        SqlCommand unosubazu;
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        //ucitavanje stvari
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            //salje komandu biranja svih vrijednosti iz pisaca koristeci vezu
+            adapter = new SqlDataAdapter(unospisaca, veza);
+
+            unosubazu = new SqlCommand("insert into Pisac(ID_pisca,Ime_pisca) values(@ID_pisca,@Ime_pisca)", veza);
+            //dodavanje podataka iz adaptera u dataset pisac
+            pisac = new DataSet();
+            adapter.Fill(pisac,"Pisac");
+            //pakistanac nije dobro objasnio ovaj dio, ako program prestane raditi vjerovatno je do ovoga
+            pregled = new DataView(pisac.Tables["Pisac"]);
+            trenutni = (CurrencyManager)this.BindingContext[pregled];
+            pregled.AddNew();
+            Navigacija();
+            prikazpozicije();
+        }
+
+        //navigacija po podacima iz baze
+        private void Navigacija()
+        {
+            //sklonimo bilo koje prosle bindove sa tekst boxova
+            textBox1.DataBindings.Clear();
+            textBox2.DataBindings.Clear();
+
+            //stavimo nas databind koji upisuje podatke iz baze u textbox
+            textBox1.DataBindings.Add("Text", pregled, "ID_pisca");
+            textBox2.DataBindings.Add("Text", pregled, "Ime_pisca");
+        }
+
+        //validacija i unos podataka u bazu
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //pretvaranje unosa korisnika u stringove
+            string ID_pisca = textBox1.Text;
+            string Ime_pisca = textBox2.Text;
+            //dodjeljivanje ovih vrijednosti parametrima unutar komande
+            unosubazu.Parameters.AddWithValue("@ID_pisca", ID_pisca);
+            unosubazu.Parameters.AddWithValue("@Ime_pisca", Ime_pisca);
+
+            //objasnjenje Regex.IsMatch:
+            // ^ - oznacava pocetak stringa
+            // \d - trazi unos broja
+            // {4} - trazi tacno 4 broja da se unesu
+            // $ - oznacava kraj stringa
+
+            //ID_pisca - slucaj kada unos nije 4 broja
+            //Luka(moj brat, ne Lazarov) mi je rekao da ovdje napisem "moj tata"...
+            if (!Regex.IsMatch(ID_pisca, @"^\d{4}$"))
+            {
+                MessageBox.Show("Morate unijeti tačno 4 broja u polje ID_pisca!", "Greška!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            //Ime_pisca - slucaj kada je prazno
+            if(string.IsNullOrEmpty(Ime_pisca))
+            {
+                MessageBox.Show("Ime_pisca ne smije biti prazno!", "Greška!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            //Ime_pisca - slucaj kada je predugo
+            if (Ime_pisca.Length > 255)
+            {
+                MessageBox.Show("Ime_pisca je predugo!", "Greška!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            if(Regex.IsMatch(ID_pisca, @"^\d{4}$") && !string.IsNullOrEmpty(Ime_pisca) && Ime_pisca.Length < 255)
+            {
+                //u slucaju da unosimo novu vrijednost u bazu
+                if (trenutni.Position == trenutni.Count - 1)
+                {
+                    try
+                    {
+                        veza.Open();
+                        unosubazu.ExecuteNonQuery();
+                        pregled.AddNew();
+                        prikazpozicije();
+                    }
+                    //ID_pisca - slucaj kada je unesena redundantna vrijednost
+                    catch (Exception redundantnost)
+                    {
+                        MessageBox.Show(redundantnost.Message, "Greška!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                //u slucaju da mjenjamo postojecu vrijednost u bazi
+                //PAZNJA!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                //OVO TREBA NAPRAVITI!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                else 
+                {
+
+                }
+            }
+        }
+
+        //prikazuje trenutnu poziciju unutar tabele pomocu label-a
+        private void prikazpozicije()
+        {
+            label1.Text = trenutni.Position+1 + ". od " + trenutni.Count.ToString();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        //dugmici za navigaciju
+        private void button2_Click(object sender, EventArgs e)
+        {
+            trenutni.Position = 0;
+            prikazpozicije();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            trenutni.Position = trenutni.Position - 1;
+            prikazpozicije();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            trenutni.Position = trenutni.Position + 1;
+            prikazpozicije();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            trenutni.Position = trenutni.Count - 1;
+            prikazpozicije();
+        }
+    }
+}
