0f1b2ea29919ebab1b79ec5c768d62e38b3b196c
Akarnik0 <brestovacnikola5@gmail.com>
2025-05-22 22:38:44 +0200

    popravljena mala greska u velicini datagridviewa u zanrpregled

--- BibliotekaNikola/Žanrpregled.Designer.cs
+++ BibliotekaNikola/Žanrpregled.Designer.cs
@@ -51,10 +51,10 @@
             // 
             this.dataGridView1.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(130)))), ((int)(((byte)(181)))), ((int)(((byte)(209)))));
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(253, 112);
+            this.dataGridView1.Location = new System.Drawing.Point(250, 112);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
-            this.dataGridView1.Size = new System.Drawing.Size(243, 159);
+            this.dataGridView1.Size = new System.Drawing.Size(252, 159);
             this.dataGridView1.TabIndex = 42;
             // 
             // button6
